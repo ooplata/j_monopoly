502c5c749ceb3c162f3378168a9dcf6cab39a26f
ooplata <ooplata@uninorte.edu.co>
2022-04-11 18:10:52 -0500

    Added fields to Card class

--- src/j_monopoly/models/Card.java
+++ src/j_monopoly/models/Card.java
@@ -1,4 +1,26 @@
 package j_monopoly.models;
 
+import j_monopoly.enums.CardType;
+
+/**
+ * Defines a card and its behavior.
+ */
 public class Card {
+    public final CardType cardType;
+
+    /**
+     * A title that uniquely identifies this card.
+     */
+    public final String title;
+
+    /**
+     * A description for this card.
+     */
+    public final String description;
+
+    public Card(CardType cardType, String title, String description) {
+        this.cardType = cardType;
+        this.title = title;
+        this.description = description;
+    }
 }
