44b4cfac1f3c43f548b2ea3606337c9f59291b63
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-21 19:23:40 -0500

    Added initial Player class

---
+++ src/j_monopoly/models/Player.java
@@ -0,0 +1,50 @@
+package j_monopoly.models;
+
+import j_monopoly.models.cards.PurchasableActionCard;
+
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class Player {
+    private final LinkedList<Property> properties = new LinkedList<>();
+    private final LinkedList<String> groups = new LinkedList<>();
+    private final LinkedList<PurchasableActionCard> actionCards = new LinkedList<>();
+
+    /**
+     * Amount of money the player has.
+     */
+    public int money = 0;
+
+    /**
+     * Amount of get out of jail free cards the player has.
+     */
+    public int outOfJailCards = 0;
+
+    /**
+     * Gets a specific property by its title.
+     * @return The property with the specified title. A null return value
+     * indicates the property is not owned.
+     */
+    public Property getPropertyByTitle(String title) {
+        for (Property prop : properties) {
+            if (Objects.equals(prop.card.title, title)) {
+                return prop;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Gets the number of properties the player owns that belong to the
+     * specified group.
+     */
+    public int ownedPropertiesInGroup(String group) {
+        int i = 0;
+        for (Property prop : properties) {
+            if (Objects.equals(prop.card.group, group)) {
+                i++;
+            }
+        }
+        return i;
+    }
+}
