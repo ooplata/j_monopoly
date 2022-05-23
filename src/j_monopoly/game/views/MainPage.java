7af066a44506cba39d18b5af8c7d1ce0e1dd8872
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-23 11:47:01 -0500

    Added initial MainPage layout

--- src/j_monopoly/game/views/MainPage.java
+++ src/j_monopoly/game/views/MainPage.java
@@ -1,6 +1,7 @@
 package j_monopoly.game.views;
 
 import javax.swing.*;
+import java.awt.*;
 
 public final class MainPage {
     public static JFrame createFrame() {
@@ -15,4 +16,23 @@ public final class MainPage {
     private JPanel root;
     private JPanel topPanel;
     private JPanel bottomPanel;
+
+    private void createUIComponents() {
+        // Top panel
+        topPanel = new JPanel();
+        topPanel.setBackground(new Color(255, 255, 255));
+        topPanel.setLayout(new GridLayout(9, 9));
+
+        Dimension topDimen = new Dimension(576, 576);
+        topPanel.setMinimumSize(topDimen);
+        topPanel.setMaximumSize(topDimen);
+
+        // Bottom panel
+        bottomPanel = new JPanel();
+        bottomPanel.setBackground(new Color(242, 242, 247));
+
+        Dimension bottomDimen = new Dimension(576, 128);
+        bottomPanel.setMinimumSize(bottomDimen);
+        bottomPanel.setMaximumSize(bottomDimen);
+    }
 }
