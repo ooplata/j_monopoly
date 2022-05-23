07e8f23d8a4629e659f659236bf67ebf15e0b0e9
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-22 23:10:38 -0500

    Added Players class with Player list

---
+++ src/j_monopoly/game/board/Players.java
@@ -0,0 +1,9 @@
+package j_monopoly.game.board;
+
+import j_monopoly.models.Player;
+
+import java.util.LinkedList;
+
+public class Players {
+    public static LinkedList<Player> players = new LinkedList<>();
+}
