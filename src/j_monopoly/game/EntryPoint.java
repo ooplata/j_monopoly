576a86e2c73809a74b3e0fbcf6b0fc62202499f9
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-23 11:24:00 -0500

    Size of MainPage is now set in entry point

--- src/j_monopoly/game/EntryPoint.java
+++ src/j_monopoly/game/EntryPoint.java
@@ -3,10 +3,15 @@ package j_monopoly.game;
 import j_monopoly.game.views.MainPage;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class EntryPoint {
     public static void main(String[] args) {
         JFrame frame = MainPage.createFrame();
+
+        frame.setSize(new Dimension(576, 640));
+        frame.setResizable(false);
+
         frame.setVisible(true);
     }
 }
