5346d5bcb39d789cdc47521398aef3eb09832aca
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-24 15:09:16 -0500

    Added methods to aid in player management

--- src/j_monopoly/game/board/Players.java
+++ src/j_monopoly/game/board/Players.java
@@ -6,4 +6,22 @@ import java.util.LinkedList;
 
 public final class Players {
     public static LinkedList<Player> players = new LinkedList<>();
+    public static int playerCount() {
+        return players.size();
+    }
+
+    public static Player getPlayerAt(int index) {
+        return players.get(index);
+    }
+
+    /**
+     * Gets the owner of a property with the given title.
+     * @return The owner if found, null otherwise.
+     */
+    public static Player getPropertyOwnerByTitle(String title) {
+        for (Player player : players)
+            if (player.getPropertyByTitle(title) != null)
+                return player;
+        return null;
+    }
 }
