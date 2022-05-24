d47f018d9eb1ed18c57b6e3b4e4fe63205be0e10
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-24 15:21:26 -0500

    Added RollResult class

---
+++ src/j_monopoly/models/RollResult.java
@@ -0,0 +1,53 @@
+package j_monopoly.models;
+
+import j_monopoly.enums.SpaceType;
+
+/**
+ * Represents the information regarding the result of a die roll
+ * and landing on a space.
+ */
+public final class RollResult {
+    /**
+     * The type of space the player landed on.
+     */
+    public final SpaceType space;
+
+    /**
+     * Result of the first die.
+     */
+    public final int firstDie;
+
+    /**
+     * Result of the second die.
+     */
+    public final int secondDie;
+
+    /**
+     * Full result of the roll.
+     */
+    public final int result;
+
+    /**
+     * Whether the player passed Go.
+     */
+    public final boolean passedGo;
+
+    /**
+     * Whether this player ended up bankrupt.
+     */
+    public final boolean isBankrupt;
+
+    public RollResult(SpaceType space,
+                      int firstDie,
+                      int secondDie,
+                      int result,
+                      boolean passedGo,
+                      boolean isBankrupt) {
+        this.space = space;
+        this.firstDie = firstDie;
+        this.secondDie = secondDie;
+        this.result = result;
+        this.passedGo = passedGo;
+        this.isBankrupt = isBankrupt;
+    }
+}
