190260b5e48c50b73c497c5593677141fab7c628
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-25 14:20:40 -0500

    Added default button

--- src/j_monopoly/game/views/MainPage.java
+++ src/j_monopoly/game/views/MainPage.java
@@ -63,6 +63,8 @@ public final class MainPage extends JFrame {
         bottomPanel.add(continueBtn);
         bottomPanel.add(exitBtn);
 
+        getRootPane().setDefaultButton(continueBtn);
+
         setupBoard();
     }
 
