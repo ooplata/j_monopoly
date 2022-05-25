ee1098bc5ae0e52fe4f26b8cbf4a87c901af8f03
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-25 00:44:37 -0500

    Updated turn dialog, added player name and extra options

--- src/j_monopoly/game/dialogs/CurrentTurnDialog.java
+++ src/j_monopoly/game/dialogs/CurrentTurnDialog.java
@@ -1,16 +1,19 @@
 package j_monopoly.game.dialogs;
 
 import j_monopoly.game.board.GameHelper;
+import j_monopoly.models.Player;
 
 import javax.swing.*;
 import java.awt.event.*;
 
 public class CurrentTurnDialog extends JDialog {
-    private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
-    private JLabel playerTurn;
-    private JComboBox optionBox;
+    private final Player player;
+
+    private final String roll = "Roll";
+    private final String checkProps = "Check properties";
+    private final String build = "Build houses";
+    private final String giveUp = "Give up";
+    private final String outOfJail = "Use out of jail card";
 
     public CurrentTurnDialog() {
         setContentPane(contentPane);
@@ -32,10 +35,25 @@ public class CurrentTurnDialog extends JDialog {
         // call onCancel() on ESCAPE
         contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
-        playerTurn.setText("Your turn, player " + (GameHelper.getCurrentPlayerIndex() + 1) + "!");
+        player = GameHelper.getCurrentPlayer();
+
+        playerTurn.setText("Your turn, " + player.name + "!");
+        money.setText("You have $" + player.money);
+
+        optionBox.addItem(roll);
+        if (!player.inJail) {
+            optionBox.addItem(checkProps);
+            optionBox.addItem(build);
+            optionBox.addItem(giveUp);
+        } else {
+            if (player.outOfJailCards > 0) {
+                optionBox.addItem(outOfJail);
+            }
+        }
     }
 
     private void onOK() {
+        GameHelper.finishTurn();
         dispose();
     }
 
@@ -48,4 +66,11 @@ public class CurrentTurnDialog extends JDialog {
         dialog.pack();
         return dialog;
     }
+
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JLabel playerTurn;
+    private JComboBox<String> optionBox;
+    private JLabel money;
 }
