5223a44e992a4a87efeec5aac411fc560638fde5
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-26 16:50:16 -0500

    Added helper class for wildcards

---
+++ src/j_monopoly/game/board/WildcardHelper.java
@@ -0,0 +1,56 @@
+package j_monopoly.game.board;
+
+import j_monopoly.assets.Resources;
+import j_monopoly.enums.CardType;
+import j_monopoly.enums.SpaceType;
+import j_monopoly.models.Property;
+import j_monopoly.models.Space;
+import j_monopoly.models.cards.ActionCard;
+import j_monopoly.models.cards.PropertyCard;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public final class WildcardHelper {
+    public static LinkedList<ActionCard> chanceCards = new LinkedList<>();
+    public static LinkedList<ActionCard> communityCards = new LinkedList<>();
+
+    /**
+     * Populates the spaces list with data from the default
+     * spaces file.
+     */
+    public static void populateList() {
+        try (InputStream chanceStrm = Resources.getResourceAsStream("Chance.txt")) {
+            BufferedReader chanceReader = new BufferedReader(new InputStreamReader(chanceStrm));
+            chanceReader.lines().forEach(WildcardHelper::addChanceCardToList);
+        } catch (IOException ignored) {
+        }
+
+        try (InputStream communityStrm = Resources.getResourceAsStream("CChest.txt")) {
+            BufferedReader communityReader = new BufferedReader(new InputStreamReader(communityStrm));
+            communityReader.lines().forEach(WildcardHelper::addCommunityCardToList);
+        } catch (IOException ignored) {
+        }
+    }
+
+    /**
+     * Gets a wildcard from an underscore ("_") separated string.
+     */
+    public static ActionCard getWildCardFromString(String str, String name) {
+        String[] split = str.split("_");
+        return new ActionCard(CardType.WILD, name, split[1], Integer.getInteger(split[0]));
+    }
+
+    private static void addChanceCardToList(String str) {
+        if (str.isEmpty() || str.startsWith("#")) return;
+        chanceCards.add(getWildCardFromString(str, "Chance"));
+    }
+
+    private static void addCommunityCardToList(String str) {
+        if (str.isEmpty() || str.startsWith("#")) return;
+        communityCards.add(getWildCardFromString(str, "Community Chest"));
+    }
+}
