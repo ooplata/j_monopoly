533a71254c2e0648d8618695559d4b39fd7434ee
ooplata <99055524+ooplata@users.noreply.github.com>
2022-05-26 20:25:06 -0500

    Added method to get app icon resource

--- src/j_monopoly/assets/Resources.java
+++ src/j_monopoly/assets/Resources.java
@@ -1,5 +1,9 @@
 package j_monopoly.assets;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Objects;
@@ -8,11 +12,24 @@ import java.util.Objects;
  * Dummy class to be able to load resources from this package.
  */
 public final class Resources {
+    private static ImageIcon icon;
+
     public static URL getResource(String name) {
         return Objects.requireNonNull(Resources.class.getResource(name));
     }
 
     public static InputStream getResourceAsStream(String name) {
         return Objects.requireNonNull(Resources.class.getResourceAsStream(name));
+    }
+
+    public static ImageIcon getAppIcon() {
+        if (icon == null) {
+            try {
+                BufferedImage pic = ImageIO.read(Resources.getResource("Token.png"));
+                icon = new ImageIcon(pic);
+            } catch (IOException ignored) {
+            }
+        }
+        return icon;
     }
 }
